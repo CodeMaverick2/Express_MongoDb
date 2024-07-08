52709c81d59a6145934cd91ffb0b653af284998c
CodeMaverick2 <codemaverick2@gmail.com>
2024-07-08 11:41:42 +0530

    MVC structured

--- index.js
+++ index.js
@@ -1,73 +1,23 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const app = express();
+require('dotenv').config()
+const productRoutes = require('./routes/productRoutes');
+const userRoutes = require('./routes/userRoutes');
 app.use(express.json());
-require('dotenv').config();
 
 mongoose
-  .connect(
-    process.env.MONGO_URL)
+  .connect(process.env.MONGO_URL)
   .then(() => {
     console.log("Db Connected");
   })
   .catch((err) => {
-    console.log("Failed" , err);
+    console.log("Db connection Failed", err);
   });
 
-const productSchema = new mongoose.Schema({
-  product_name : {
-    type : String,
-    required : true
-  },
-  price : {
-    type : String,
-    required : true
-  },
-  isInStock : {
-    type : Boolean,
-    required : true
-  },
-  Category : {
-    type : String,
-    required : true
-  }
-});
 
-const productModel = mongoose.model('products',productSchema);
-//create
-app.post("/api/products", async (req, res) => {
-  const product = productModel.create({
-    product_name : req.body.product_name,
-    price : req.body.price,
-    isInStock : req.body.isInStock,
-    Category : req.body.Category
-  });
-  console.log(product);
-  return res.status(201).json({message : "Product created successfully"});
-});
-
-app.get('/api/products' , async(req , res)=>{
-  const allProucts = await productModel.find({isInStock:true})
-
-  return res.json(allProucts)
-})
-
-// Get product by id
-
-app.get('/api/products/:id' , async(req , res)=>{
-const product = await productModel.findById(req.params.id)
-
-return res.json(product)
-})
-
-// Update product
-
-app.put('/api/products/:id' , async(req , res)=>{
- const updatedProduct = await productModel.findByIdAndUpdate(req.params.id , req.body)
- return res.json(updatedProduct)
-})
-
-const PORT = 3000;
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+app.use('/api/products' , productRoutes);
+app.use('/api/users', userRoutes);
+app.listen(8086, () => {
+  console.log("Server sarted at port 8086");
 });
